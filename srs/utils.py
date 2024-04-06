bff94ff9a0316a5411e5803449688a1d753ae671
EvgeniyMaya <super89048368898@yandex.ry>
2024-04-06 21:23:40 +0400

    ok2

--- srs/utils.py
+++ srs/utils.py
@@ -1,4 +1,4 @@
-import json,os
+import json, os
 import datetime as dt
 
 a = os.path.abspath('operations.json')
@@ -11,35 +11,32 @@ def open_json_file(file_path):
 
 
 def filter_operations(operation_data):
-    filtered_list=[]
+    filtered_list = []
     for operation in operation_data:
-        if operation.get("state") =="EXECUTED":
+        if operation.get("state") == "EXECUTED":
             filtered_list.append(operation)
     return filtered_list
 
 
-
-
-def sort_operration(operation_data: list[dict])->list[dict]:
-    sor_list = sorted(operation_data,key=lambda x: x["date"],reverse=True)
+def sort_operration(operation_data: list[dict]) -> list[dict]:
+    sor_list = sorted(operation_data, key=lambda x: x["date"], reverse=True)
     return sor_list
 
 
-
 def mask_operation_info(operation):
-    operation_to = operation.get("to")
     operation_from = operation.get("from")
 
     if operation_from:
         parts = operation_from.split()
-        numbers =parts[-1]
-        if len(numbers) ==16:
-            masked_numbers =f"{numbers[:4]}{numbers[4:6]}** **** {numbers[-4:]}"
+        numbers = parts[-1]
+        if len(numbers) == 16:
+            masked_numbers = f"{numbers[:4]}{numbers[4:6]}** **** {numbers[-4:]}"
             return f"{" ".join(parts[:-1])} {masked_numbers} ->"
         else:
-             return f"Счет **{numbers[-4:]} "
+            return f"Счет **{numbers[-4:]} ->"
 
     return "->"
+
 
 def operation_to(operation):
     operation_to = operation.get("to")
@@ -50,8 +47,6 @@ def operation_to(operation):
 
 
 def format_date(operation):
-    date:str =operation["date"]
-    dt_time: dt.datetime = dt.datetime.strptime(date,'%Y-%m-%dT%H:%M:%S.%f')
+    date: str = operation["date"]
+    dt_time: dt.datetime = dt.datetime.strptime(date, '%Y-%m-%dT%H:%M:%S.%f')
     return dt_time.strftime("%d.%m.%Y")
-
-
